3f098197970a3e2d2f85c0836ad60154cc5626e8
ccik2285 <ccik1653@gmail.com>
2025-03-23 19:50:37 +0900

    solution

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] coins, int sum) {
+        int[] dp = new int[sum + 1];
+
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= sum; i++) {
+                dp[i] += dp[i - coin];–
+            }
+        }
+
+        return dp[sum];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1,2,3};
+        int sum = 4;
+
+        System.out.println(solution(coins, sum));
+    }
+} 
